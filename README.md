feb556b1eadf24d58c8daaa8ce997e27b72021e2
Sam Anttila <the@netsec.expert>
2021-11-04 11:00:24 +0000

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # dump-scripts
+
+Dumps all scripts on a page to a local directory, including inline scripts.
+
+
+## Usage
+
+`usage: dump-scripts.py [-h] [--useragent USERAGENT] URL`
+
+e.g. 
+
+`./dump-scripts.py https://reddit.com`
+`python3 dump-scripts.py --useragent="haxbot v1337" https://reddit.com`
+
+After which you will have a folder called "scripts" underneath the directory the dump-scripts.py file resides in containing all scripts found on said page.
+
---
+++ dump-scripts.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import requests
+from bs4 import BeautifulSoup as bs
+import os
+import hashlib
+import argparse
+
+
+"""Simple python utility to dump all scripts from a given page."""
+
+def download_script(url,downloads_dir_path):
+    """Download script into given directory. Note: Does nothing to avoid name collisions"""
+    local_filename = url.split("/")[-1]
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(os.path.join(downloads_dir_path,local_filename), 'wb+') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk:
+                f.write(chunk)
+    return local_filename
+
+
+def ensure_dir(file_path):
+    """Ensure directory exists by creating it if not present"""
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+if __name__ == "__main__":
+    downloads_dirname = "scripts/"
+    curr_path = os.path.dirname(os.path.realpath(__file__))
+    downloads_path = os.path.join(curr_path,downloads_dirname)
+    ensure_dir(downloads_path)
+
+    parser = argparse.ArgumentParser(description='Download all scripts from a website into a scripts/ folder underneath this script')
+    parser.add_argument('url', metavar='URL', type=str, help='The url (including schema) from which to dump scripts')
+    parser.add_argument('--useragent', dest='useragent', type=str, default="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36", help='User agent to use when making requests')
+    args = parser.parse_args()
+
+    res = requests.get(args.url,headers={"User-Agent":args.useragent})
+    soup = bs(res.text,features="html.parser")
+    for script in soup.find_all("script"):
+        if "src" in script.attrs:
+            pass
+            #externally loaded script
+            print(f'Downloaded {download_script(script.attrs["src"],downloads_path)}')
+        else:
+            #inline script
+            print(script.text)
+            m = hashlib.sha256()
+            m.update(script.text.encode("utf32"))
+            local_filename = m.hexdigest() # To give all inline scripts a unique name we take the hash of its contents. Only identical scripts should collide.
+            with open(os.path.join(downloads_path,local_filename)+".js", 'w+') as f:
+                f.write(script.text)
+    print("Done downloading scripts. They should be under the 'scripts/' folder.")
+
+
+
